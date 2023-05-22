64d23adaa12e6afa20044fd7ac966a6cefd98cb3
Egenieee <hhgg0925@naver.com>
2023-05-22 23:26:27 +0900

    feat: UsersRepository 생성

---
+++ src/main/java/com/betterme/repository/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.betterme.repository;
+
+import com.betterme.domain.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
