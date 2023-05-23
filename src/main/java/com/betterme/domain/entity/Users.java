723bb956b848631f8fbb927d9270adb14c40917c
Egenieee <hhgg0925@naver.com>
2023-05-23 16:13:06 +0900

    feat: 회원 엔티티 생성

---
+++ src/main/java/com/betterme/domain/entity/Users.java
@@ -0,0 +1,35 @@
+package com.betterme.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    @Column
+    private String password;
+
+    private String email;
+
+    @Column
+    private String slogan;
+
+    @Builder
+    public Users(String name, String email, String password, String slogan) {
+        this.userName = name;
+        this.email = email;
+        this.password = password;
+        this.slogan = slogan;
+    }
+
+}
