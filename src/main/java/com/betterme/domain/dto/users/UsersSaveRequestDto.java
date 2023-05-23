0499d1fed235438b5553bf5943e62b453e31ac1c
Egenieee <hhgg0925@naver.com>
2023-05-23 16:13:48 +0900

    feat: 회원 생성 dto 생성

---
+++ src/main/java/com/betterme/domain/dto/users/UsersSaveRequestDto.java
@@ -0,0 +1,48 @@
+package com.betterme.domain.dto.users;
+
+import com.betterme.domain.entity.Users;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UsersSaveRequestDto {
+
+    //    @NotBlank(message = "회원 아이디는 필수입니다.")
+    @Length(min = 6, max = 15, message = "6 ~ 15 길이로 입력해야 합니다.")
+    private String userName;
+
+    //    @NotBlank
+    @Email(message = "이메일 형식을 확인해주세요.")
+    @NotBlank
+    private String email;
+
+    private String slogan;
+
+    //    @NotBlank
+    @Length(min = 6, max = 15, message = "6 ~ 15 길이로 입력해야 합니다.")
+    private String password;
+
+    @Builder
+    public UsersSaveRequestDto(String userName, String email, String slogan, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.slogan = slogan;
+        this.password = password;
+    }
+
+    public Users toEntity() {
+        return Users.builder()
+                .name(userName)
+                .email(email)
+                .password(password)
+                .slogan(slogan)
+                .build();
+    }
+}
