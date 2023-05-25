bd416da5d81bc574df033763305fbf922dd3aab3
Egenieee <hhgg0925@naver.com>
2023-05-25 16:11:46 +0900

    feat: 중복 아이디 체크 로직 추가

--- src/main/java/com/betterme/repository/UsersRepository.java
+++ src/main/java/com/betterme/repository/UsersRepository.java
@@ -4,4 +4,5 @@ import com.betterme.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    boolean existsUsersByUserName(String userName);
 }
--- src/main/java/com/betterme/service/UsersService.java
+++ src/main/java/com/betterme/service/UsersService.java
@@ -1,8 +1,10 @@
 package com.betterme.service;
 
 import com.betterme.domain.dto.users.UsersSaveRequestDto;
+import com.betterme.exception.UsersNotUniqueException;
 import com.betterme.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void save(UsersSaveRequestDto requestDto) {
-        usersRepository.save(requestDto.toEntity());
+        // 사용자 아이디 중복 체크
+        if (isExistUserName(requestDto.getUserName())) {
+            throw new UsersNotUniqueException("이미 등록된 사용자 입니다.");
+        }
+
+        // 비밀번호 인코딩
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+
+        // 유저 저장
+        usersRepository.save(requestDto.toEntity(encodedPassword));
+    }
+
+    private boolean isExistUserName(String userName) {
+        return usersRepository.existsUsersByUserName(userName);
     }
 }
