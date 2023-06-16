510e606fe843a6bdcf21bd3e308316c3a0e42191
Egenieee <hhgg0925@naver.com>
2023-06-16 12:11:47 +0900

    style: Better Me 화면에 날짜 표시, 삭제 버튼 수정

--- src/main/java/com/betterme/domain/dto/betterme/BetterMeResponseDto.java
+++ src/main/java/com/betterme/domain/dto/betterme/BetterMeResponseDto.java
@@ -4,6 +4,7 @@ import com.betterme.domain.entity.BetterMe;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Getter
@@ -11,6 +12,7 @@ public class BetterMeResponseDto {
 
     private final Long id;
     private final String usersName;
+    private final String date;
     private final float progress;
     private final Map<String, String> habits;
 
@@ -18,6 +20,7 @@ public class BetterMeResponseDto {
     public BetterMeResponseDto(BetterMe betterMe) {
         this.id = betterMe.getId();
         this.usersName = betterMe.getUsers().getUsersName();
+        this.date = betterMe.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일"));
         this.habits = betterMe.getHabitsAndUrl();
         this.progress = betterMe.getProgress();
     }
