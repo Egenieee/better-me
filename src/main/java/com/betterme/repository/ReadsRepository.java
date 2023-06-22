64b22b0c2313b1e9f7e41783af2b4276a31d1e60
Egenieee <hhgg0925@naver.com>
2023-06-22 16:32:24 +0900

    feat: ReadsRepository 생성

---
+++ src/main/java/com/betterme/repository/ReadsRepository.java
@@ -0,0 +1,7 @@
+package com.betterme.repository;
+
+import com.betterme.domain.entity.Reads;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadsRepository extends JpaRepository<Reads, Long> {
+}
